384b2126ff8efaacc5ede7e9a4f4591ffebb0c7f
Kristin Barr <kristinbarr@gmail.com>
2019-10-16 12:05:38 -0400

    added generate token in router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 
 const Users = require('../users/users-model.js');
+const secrets = require('../config/secrets')
 
 // for endpoints beginning with /api/auth
 router.post('/register', (req, res) => {
@@ -24,9 +26,14 @@ router.post('/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
+
       if (user && bcrypt.compareSync(password, user.password)) {
+        // library previous will generate token automatically
+        const token = generateToken(user)
+
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token,
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -36,5 +43,23 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+// generate token user
+function generateToken(user) {
+  // invoke the JWT sign method
+  // at least 3 params, secret used to decrypt token
+  const payload = { // some props are standard: subject, username
+    subject: user.id, // sub
+    username: user.username,
+    // ...other data
+  }
+  // no longer needed when we make secrets file
+  // const secret = 'wsfkmwefodfcwlkemfrkf'
+  const options = {
+    expiresIn: '8h' // more options in library: npmjs.com
+  }
+
+  return jwt.sign(payload, secrets.jwtSecret, options)
+}
 
 module.exports = router;
